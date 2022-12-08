0e61a1ee769e63dd218202273102525e5df61526
Abel-desu <95746961+Abel-desu@users.noreply.github.com>
2022-12-08 11:27:20 +0300

    Create API and controllers

---
+++ app/javascript/application.js
@@ -0,0 +1,17 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from "react-redux";
+import App from './app';
+import store from './redux/configureStore';
+
+
+
+ReactDOM.render(
+    <Provider store={store}>
+       <App/>
+    </Provider>,
+  document.getElementById('root'),
+);
